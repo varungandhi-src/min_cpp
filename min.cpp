47eaba0154189e40c44ae22e3b07f1084f87cf90
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-04-10 10:11:07 +0900

    tweak some stuff

--- min.cpp
+++ min.cpp
@@ -10,10 +10,17 @@ struct A { };
 
 using namespace std;
 
+int global = 10;
+
 void f() {
   A<S> t{};
   A<int> a{};
   vector<int> x;
   std::vector<S> z;
   std::unique_ptr<int> y;
+  int local = global;
+
+
+
+  int otherlocal = local;
 }
